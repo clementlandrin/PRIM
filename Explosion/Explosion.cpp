8b231e4fae1a048e2a3d147cff314d72ba0df641
clandrin <clement.landrin@telecom-paristech.fr>
2020-01-10 18:25:00 +0100

    Added ray tracer. To use: press enter when paused with space.

--- Explosion/Explosion.cpp
+++ Explosion/Explosion.cpp
@@ -8,6 +8,13 @@
 #include <GL/glew.h>
 #include <GL/glut.h>
 
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+
+
 #include "Particle.hpp"
 #include "Fluid.hpp"
 #include "OctreeNode.hpp"
@@ -17,6 +24,9 @@
 
 #define GLUT_SPACE 32
 #define GLUT_ESC 27
+#define GLUT_ENTER 13
+
+#define M_PI 3.14159
 
 #define VISCOSITY 10000.0f
 #define DENSITY 1.0f
@@ -44,6 +54,8 @@ static float initialTime;
 static float zeroTimeOfSimulation;
 
 int actual_power_of_two_resolution;
+
+void computeRayTracedImage();
 
 struct LightSource
 {
@@ -158,6 +170,12 @@ void keyboardCallback(unsigned char key, int x, int y)
 	case GLUT_ESC:
 		exit(0);
 		break;
+	case GLUT_ENTER:
+		if (pause)
+		{
+			computeRayTracedImage();
+			break;
+		}
 	}
 }
 
@@ -908,6 +926,284 @@ void clear()
 	}
 	delete octreeRoot;
 	delete fluid;
+}
+
+struct Vec
+{
+	double x, y, z;
+	Vec(double x_ = 0, double y_ = 0, double z_ = 0)
+	{
+		x = x_;
+		y = y_;
+		z = z_;
+	}
+	Vec operator+(const Vec &b) const { return Vec(x + b.x, y + b.y, z + b.z); }
+	Vec operator-(const Vec &b) const { return Vec(x - b.x, y - b.y, z - b.z); }
+	Vec operator*(double b) const { return Vec(x * b, y * b, z * b); }
+	Vec mult(const Vec &b) const { return Vec(x * b.x, y * b.y, z * b.z); }
+	Vec &normalize() { return *this = *this * (1 / sqrt(x * x + y * y + z * z)); }
+	double dot(const Vec &b) const { return x * b.x + y * b.y + z * b.z; }
+	Vec cross(Vec &b) { return Vec(y * b.z - z * b.y, z * b.x - x * b.z, x * b.y - y * b.x); }
+};
+Vec operator*(double b, Vec const &o) { return Vec(o.x * b, o.y * b, o.z * b); }
+
+void generateRandomPointOnSphere(double &theta, double &phi)
+{
+	double x = (double)(rand()) / RAND_MAX;
+	double y = (double)(rand()) / RAND_MAX;
+	theta = x * 2.0 * M_PI;
+	phi = acos(std::min<double>(1.0, std::max<double>(-1.0, 2.0 * y - 1.0)));
+}
+Vec randomSampleOnSphere()
+{
+	double theta, phi;
+	generateRandomPointOnSphere(theta, phi);
+	return Vec(cos(theta) * cos(phi), sin(theta) * cos(phi), sin(phi));
+}
+Vec randomSampleOnHemisphere(Vec const &upDirection)
+{
+	Vec r = randomSampleOnSphere();
+	if (r.dot(upDirection) > 0.0)
+		return r;
+	return -1.0 * r;
+}
+
+struct Ray
+{
+	Vec o, d;
+	Ray(Vec o_, Vec d_) : o(o_), d(d_)
+	{
+		d.normalize();
+	}
+};
+
+enum Refl_t
+{
+	DIFFUSE,
+	MIRROR,
+	GLASS,
+	EMMISSIVE
+}; // material types, used in radiance()
+
+struct Sphere
+{
+	double radius; // radius
+	Vec p, e, c;   // position, emission, color
+	Refl_t refl;   // reflection type (DIFFuse, SPECular, REFRactive)
+	Sphere(double rad_, Vec p_, Vec e_, Vec c_, Refl_t refl_) : radius(rad_), p(p_), e(e_), c(c_), refl(refl_) {}
+
+	double intersect(const Ray &r) const
+	{ // returns distance, 0 if nohit
+		// TODO
+		Vec oc = r.o - p;
+		double sa = 1.0;
+		double sb = 2.0 * oc.dot(r.d);
+		double sc = oc.dot(oc) - radius * radius;
+
+		double delta = sb * sb - 4.0 * sa * sc;
+		if (delta < 0.0)
+			return 0.0; // no solution
+
+		double deltaSqrt = sqrt(delta);
+		double lambda1 = (-sb - deltaSqrt) / (2.0 * sa);
+		double lambda2 = (-sb + deltaSqrt) / (2.0 * sa);
+		if (lambda1 < lambda2 && lambda1 >= 0.0)
+			return lambda1;
+		if (lambda2 >= 0.0)
+			return lambda2;
+		return 0.0;
+	}
+
+	Vec randomSample() const
+	{
+		return p + radius * randomSampleOnSphere();
+	}
+};
+
+// Scene :
+std::vector<Sphere> spheres;
+std::vector<unsigned int> lights;
+// lights is the whole set of emissive objects
+
+inline double clamp(double x) { return x < 0 ? 0 : x > 1 ? 1 : x; }
+inline double clamp(double x, double min, double max)
+{
+	if (x < min)
+		x = min;
+	else if (x > max)
+		x = max;
+	return x;
+}
+inline bool intersectScene(const Ray &r, double &t, int &id)
+{
+	double d, inf = t = 1e20;
+	for (int i = 0; i < spheres.size(); ++i)
+		if ((d = spheres[i].intersect(r)) && d < t)
+		{
+			t = d;
+			id = i;
+		}
+	return t < inf;
+}
+
+double brdf(const Vec &wi, const Vec &n, const Vec &wo)
+{
+	const double Kd = 1.0;
+	const double Ks = 0.0;
+	const double s = 10.0; // brightness
+
+	Vec r = 2 * n * (wi.dot(n)) - wi;
+	double fs = Ks * pow(r.dot(wo), s);
+
+	double brdf = Kd + fs;
+	return brdf;
+}
+
+Vec refract(const Vec &I, const Vec &N, const float &ior)
+{
+	float cosi = clamp(-1, 1, I.dot(N));
+	float etai = 1, etat = ior;
+	Vec n = N;
+	if (cosi < 0)
+		cosi = -cosi;
+	else
+	{
+		std::swap(etai, etat);
+		n = -1.0 * N;
+	}
+	float eta = etai / etat;
+	float k = 1 - eta * eta * (1 - cosi * cosi);
+	return k < 0 ? 0 : eta * I + (eta * cosi - sqrtf(k)) * n;
+}
+
+Vec radiance(const Ray &r, int depth)
+{
+	double t;   // distance to intersection
+	int id = 0; // id of intersected object
+	if (!intersectScene(r, t, id))
+		return Vec(); // if miss, return black
+
+	const Sphere &obj = spheres[id]; // the hit object
+
+	Vec x = r.o + r.d * t,           // the hit position
+		n = (x - obj.p).normalize(), // the normal of the object at the hit
+		f = obj.c;                   // the color of the object at the hit
+
+	if (n.dot(r.d) > 0.0)
+		n = -1.0 * n;
+
+	if (++depth > 5)
+		return Vec(); // we limit the number of rebounds in the scene
+
+	if (obj.refl == EMMISSIVE)
+	{ // we hit a light
+		return obj.e;
+	}
+	if (obj.refl == DIFFUSE)
+	{ // Ideal DIFFUSE reflection
+		// We shoot rays towards all lights:
+		Vec rad;
+		for (unsigned int lightIt = 0; lightIt < lights.size(); ++lightIt)
+		{
+			const Sphere &light = spheres[lights[lightIt]];
+
+			// TODO
+			const Vec dirToLight = (light.randomSample() - x).normalize();
+			const Ray &toLight = Ray(x + 0.0001 * dirToLight, dirToLight);
+			int idTemp;
+			double tTemp;
+			if (intersectScene(toLight, tTemp, idTemp))
+			{
+				if (idTemp == lights[lightIt])
+				{
+					const Vec Li = light.e;
+					rad = rad + Li.mult(obj.c) * brdf(toLight.d, n, -1.0 * r.d) * (n.dot(toLight.d));
+				}
+			}
+		}
+
+		// TODO : add secondary rays:
+		const uint32_t NB_SECONDARY_RAY = 0;
+		for (uint32_t i = 0; i < NB_SECONDARY_RAY; i++)
+		{
+			const Vec newDir = randomSampleOnHemisphere(n).normalize();
+			const Ray &newRay = Ray(x + 0.0001 * newDir, newDir);
+			rad = rad + radiance(newRay, depth) * brdf(newRay.d, n, -1.0 * r.d) * (n.dot(newRay.d)); // FIXME : Have to be normalized
+		}
+
+		return rad;
+	}
+	else if (obj.refl == MIRROR)
+	{ // Ideal SPECULAR reflection
+		// TODO
+		Vec dirToReflected = r.d - 2 * n * (r.d.dot(n));
+		const Ray &toReflected = Ray(x + 0.0001 * dirToReflected, dirToReflected);
+
+		return radiance(toReflected, depth).mult(obj.c);
+	}
+	else if (obj.refl == GLASS)
+	{ // Ideal SPECULAR refraction
+		// TODO
+
+		Vec dirToRefracted = refract(r.d, n, 1.500);
+		const Ray &toReflected = Ray(x + 0.0001 * dirToRefracted, dirToRefracted);
+
+		return radiance(toReflected, depth).mult(obj.c);
+		return Vec();
+	}
+
+	return Vec();
+}
+
+
+void computeRayTracedImage()
+{
+	int w = 1024, h = 768, samps = 1; // # samples
+	Ray cam(Vec(50, 52, 295.6), Vec(0, -0.042612, -1).normalize());   // camera center and direction
+	Vec cx = Vec(w * .5135 / h), cy = (cx.cross(cam.d)).normalize() * .5135, *pixelsColor = new Vec[w * h];
+
+	// setup scene:
+	spheres.push_back(Sphere(1e5, Vec(1e5 + 1, 40.8, 81.6), Vec(0, 0, 0), Vec(.75, .25, .25), DIFFUSE));   //Left
+	spheres.push_back(Sphere(1e5, Vec(-1e5 + 99, 40.8, 81.6), Vec(0, 0, 0), Vec(.25, .25, .75), DIFFUSE)); //Rght
+	spheres.push_back(Sphere(1e5, Vec(50, 40.8, 1e5), Vec(0, 0, 0), Vec(.75, .75, .75), DIFFUSE));         //Back
+	spheres.push_back(Sphere(1e5, Vec(50, 40.8, -1e5 + 170), Vec(0, 0, 0), Vec(0, 0, 0), DIFFUSE));        //Front
+	spheres.push_back(Sphere(1e5, Vec(50, 1e5, 81.6), Vec(0, 0, 0), Vec(.75, .75, .75), DIFFUSE));         //Bottom
+	spheres.push_back(Sphere(1e5, Vec(50, -1e5 + 81.6, 81.6), Vec(0, 0, 0), Vec(.75, .75, .75), DIFFUSE)); //Top
+	spheres.push_back(Sphere(16.5, Vec(27, 16.5, 47), Vec(0, 0, 0), Vec(1, 1, 1) * .999, MIRROR));         //Mirr
+	spheres.push_back(Sphere(16.5, Vec(73, 16.5, 78), Vec(0, 0, 0), Vec(1, 1, 1) * .999, GLASS));         //Change to Glass
+	spheres.push_back(Sphere(5, Vec(50, 70, 50), Vec(1, 1, 1), Vec(0, 0, 0), EMMISSIVE));                  //Light
+	lights.push_back(8);
+
+	// ray trace:
+	for (int y = 0; y < h; y++)
+	{ // Loop over image rows
+		fprintf(stderr, "\rRendering (%d spp) %5.2f%%", samps * 4, 100. * y / (h - 1));
+		for (unsigned short x = 0; x < w; x++)
+		{ // Loop cols
+			Vec r(0, 0, 0);
+			for (unsigned int sampleIt = 0; sampleIt < samps; ++sampleIt)
+			{
+				double dx = ((double)(rand()) / RAND_MAX);
+				double dy = ((double)(rand()) / RAND_MAX);
+				Vec d = cx * ((x + dx) / w - .5) +
+					cy * ((y + dy) / h - .5) + cam.d;
+				r = r + radiance(Ray(cam.o + d * 140, d.normalize()), 0) * (1. / samps);
+			}
+
+			pixelsColor[x + (h - 1 - y) * w] = pixelsColor[x + (h - 1 - y) * w] + Vec(clamp(r.x), clamp(r.y), clamp(r.z));
+		} // Camera rays are pushed ^^^^^ forward to start in interior
+	}
+
+	// save image:
+	FILE *f;
+	errno_t err = fopen_s(&f, "image.ppm", "w"); // Write image to PPM file.
+	if (err == 0)
+	{
+		fprintf(f, "P3\n%d %d\n%d\n", w, h, 255);
+		for (int i = 0; i < w * h; i++)
+			fprintf(f, "%d %d %d ", (int)(pixelsColor[i].x * 255), (int)(pixelsColor[i].y * 255), (int)(pixelsColor[i].z * 255));
+		fclose(f);
+	}
 }
 
 int main(int argc, char **argv) {
